0ca91bb32d0d5f8b73d790eb2dbe2c7b52eb6a16
Julien <julien.bailly4@etu.univ-lorraine.fr>
2018-11-09 16:00:20 +0100

    modif diagramme v2.1 & ajout classes vides cases spéciales

---
+++ src/model/Passage.java
@@ -0,0 +1,21 @@
+package model;
+
+import java.awt.Graphics2D;
+
+public class Passage extends Special {
+
+	public Passage(int x, int y) {
+		super(x, y);
+	}
+
+	@Override
+	public void trigger(Entite entite) {
+
+	}
+
+	@Override
+	public void dessiner(Graphics2D g) {
+
+	}
+
+}
---
+++ src/model/PiegeAOurs.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.awt.Graphics2D;
+
+public class PiegeAOurs extends Special {
+	
+	public final static int DEGATSPIEGEOURS = 5;
+
+	public PiegeAOurs(int x, int y) {
+		super(x, y);
+	}
+
+	@Override
+	public void trigger(Entite entite) {
+				
+	}
+
+	@Override
+	public void dessiner(Graphics2D g) {
+		
+	}
+
+}
