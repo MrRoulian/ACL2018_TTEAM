373d9efdfd7c10eeba5aad9e19c4d08e09e0d720
Julien <julien.bailly4@etu.univ-lorraine.fr>
2018-11-16 16:21:12 +0100

    ralentissement des mobs

--- src/model/Aleatoire.java
+++ src/model/Aleatoire.java
@@ -3,10 +3,18 @@ package model;
 import moteurJeu.Commande;
 
 public class Aleatoire implements Comportement {
+	
+	private int slow = 0;
+	private static final int TEMPS_ENTRE_ACTION = 5;
 
 	@Override
 	public Commande bouger() {
 		Commande c = new Commande();
+		slow++;
+		if (slow%TEMPS_ENTRE_ACTION!=0){
+			return c;
+		}
+		slow%=TEMPS_ENTRE_ACTION;
 		switch((int)(Math.random()*4)){
 			case 0:
 				c.haut = true;
