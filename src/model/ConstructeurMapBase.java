6a9a86deb5a49abb8c42c2d32107aea48f043858
Koringar <alexisbarthelemy54@yahoo.fr>
2018-11-16 14:32:31 +0100

    Commentaire et javadoc des classe case constructeuret entite

--- src/model/ConstructeurMapBase.java
+++ src/model/ConstructeurMapBase.java
@@ -2,36 +2,59 @@ package model;
 
 public class ConstructeurMapBase implements ConstructeurMap{
 
+	/**
+	 * Instance du generateur pour eviter la multiplication d'instance
+	 */
 	private static ConstructeurMapBase instance;
 	
+	/**
+	 * Constructeur prive pour eviter la creation de noveaux objet en dehors de notre instance
+	 */
 	private ConstructeurMapBase() {}
 	
+	/**
+	 * Methode permetant de recuperer l'instance du ConstructeurMapBAse
+	 * @return Instance de ConstructeurMapBase
+	 */
 	public static ConstructeurMap getInstance() {
+		// Si l'instance n'est pas instanciee on l'instancie
 		if (instance == null) {
 			instance = new ConstructeurMapBase();
 		}
 		return instance;
 	}
-	
+
+	@Override
 	public Case[][] newMap(int level) throws GenerationException {
+		// Initialisation de la matrice 
 		Case[][] map = new Case[level*5][level*5];
+		// Recuperation de la taille de la matrice
 		int width = map.length;
 		int height = map[0].length;
-		//Creation de l'arène
+		//Creation de l'arène (tout les bord de la matrice sont des murs eet le centre est du sol)
 		for(int i=0;i<width;i++){
 			for(int j=0;j<height;j++){
 				map[i][j]=( i == 0 || i == width-1 || j == 0 || j == height-1)? new Mur(i,j) : new Sol(i,j);
 			}
 		}		
 		
+		// Placement des tresor 
 		placerTresor(map, level);
+		
 		return map;
 	}
 	
+	/**
+	 * Methode qui place un tresor dans la map 
+	 * @param map Map auquel on ajoute le tresor 
+	 * @param level Niveau pour change le nombre de tresor a mettre dans la map
+	 */
 	private void placerTresor(Case[][] map, int level) {
+		// On place des tresor en diagonale
 		for (int i = 0; i < level; i++) {
 			map[1+i*2][1+i*2] = new Tresor(1+i*2, 1+i*2);
 		}
+		// On augmente le nombre de tresor dans le labyrinthe
 		Labyrinthe.nbTreasureLeft = level;
 	}
 }
--- src/model/Entite.java
+++ src/model/Entite.java
@@ -3,18 +3,39 @@ package model;
 import java.awt.Graphics2D;
 
 import moteurJeu.Commande;
-
+/**
+ * Classe abstraite representant le comportement d'une entite vivate dans le jeu
+ */
 public abstract class Entite {
 
+	/**
+	 * Entier correspondant a la position de l'entite 
+	 */
 	protected int x, y;
-	protected boolean avoidSolidCase, triggerCase;
+	/**
+	 * Booleen indicant si l'entite a la capacite de traverser les cases normalement pas traversable
+	 */
+	protected boolean avoidSolidCase;
+	/**
+	 * Booleen indicant si l'entite active les cases lorsqu'elle marche dessus
+	 */
+	protected boolean triggerCase;
 
+	/**
+	 * Map dans laquel notre entite evolue
+	 */
 	protected Labyrinthe labyrinthe;
-
-	//protectd Sprite sprite;
-
+	
+	/**
+	 * Permet au moteur de mettre a jour l'entite a chaque iteration du jeu 
+	 * @param commande Commande representant l'input clavier a l'iteration du jeu 
+	 */
 	protected abstract void update(Commande commande);
 
+	/**
+	 * Methode qui permet de dessiner l'entite sur l'image a construire
+	 * @param g Graphics sur lequel on dessine l'image
+	 */
 	protected abstract void dessiner(Graphics2D g);
 
 	public int getX() {
@@ -25,7 +46,11 @@ public abstract class Entite {
 		return y;
 	}
 
+	/**
+	 * Methode qui deplace l'entite sur la droite
+	 */
 	public void droite() {
+		// Si l'entite ne peut traverser les cases on teste la collision sinon on deplace vers la droite
 		if (!avoidSolidCase) {
 			if(labyrinthe.getCase(++x, y).isSolid()) {
 				this.x--;
@@ -33,12 +58,17 @@ public abstract class Entite {
 		} else {
 			++x;
 		}
+		// Si l'entite declanche les cases on declanche la case sur lequel il passe 
 		if (triggerCase) {
 			labyrinthe.getCase(x, y).trigger(this);
 		}
 	}
 	
+	/**
+	 * Methode qui deplace l'entite sur la gauche
+	 */
 	public void gauche() {
+		// Si l'entite ne peut traverser les cases on teste la collision sinon on deplace vers la gauche
 		if (!avoidSolidCase) {
 			if(labyrinthe.getCase(--x, y).isSolid()) {
 				this.x++;
@@ -46,12 +76,17 @@ public abstract class Entite {
 		} else {
 			--x;
 		}
+		// Si l'entite declanche les cases on declanche la case sur lequel il passe 
 		if (triggerCase) {
 			labyrinthe.getCase(x, y).trigger(this);
 		}
 	}
 	
+	/**
+	 * Methode qui deplace l'entite vers le haut
+	 */
 	public void haut() {
+		// Si l'entite ne peut traverser les cases on teste la collision sinon on deplace vers le haut
 		if (!avoidSolidCase) {
 			if(labyrinthe.getCase(x, --y).isSolid()) {
 				this.y++;
@@ -59,12 +94,17 @@ public abstract class Entite {
 		} else {
 			--y;
 		}
+		// Si l'entite declanche les cases on declanche la case sur lequel il passe 
 		if (triggerCase) {
 			labyrinthe.getCase(x, y).trigger(this);
 		}
 	}
 	
+	/**
+	 * Methode qui deplace l'entite vers le bas
+	 */
 	public void bas() {
+		// Si l'entite ne peut traverser les cases on teste la collision sinon on deplace vers le bas
 		if (!avoidSolidCase) {
 			if(labyrinthe.getCase(x, ++y).isSolid()) {
 				this.y--;
@@ -72,6 +112,7 @@ public abstract class Entite {
 		} else {
 			++y;
 		}
+		// Si l'entite declanche les cases on declanche la case sur lequel il passe 
 		if (triggerCase) {
 			labyrinthe.getCase(x, y).trigger(this);
 		}
